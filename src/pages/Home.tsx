80d4854123ca5f1e1507bd050d6d04e3dc26ca17
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-05-12 22:33:34 +0900

    画面遷移作成

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Upload from './Upload';
+
+const Home = () => {
+  return (
+    <>
+      <h1>Home！</h1>
+      <a href="/upload">Upload</a>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/pages/Upload.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Upload = () => {
+  return (
+    <>
+      <h1>Upload!</h1>
+      <a href="/">Home</a>
+    </>
+  );
+};
+
+export default Upload;
