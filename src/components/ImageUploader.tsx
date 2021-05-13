8eaf1f1389cdbed268222a5bf1217bbc76cc677d
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-05-14 07:11:05 +0900

    [add] 拡張子を自動で変更できるように修正

--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -22,15 +22,21 @@ const ImageUploader = (): JSX.Element => {
     if (Object.values(preview)[0] === '') return null;
 
     // ファイルが選択されている場合、表示させる
-    // ※ Firebase Storage のルールを全て許可してしまったので、認証機能実装後に
-    // ※ ルールの見直しをする必要がある
+    // ※ Firebase Storage のルールを全て許可してしまったので（allow read, write: if true;）
+    // ※ 認証機能実装後にルールの見直しをする必要がある
     const onUploadImage = async () => {
       // Data URI を Blob に変換する
-      const response = await fetch(preview.preview);
+      const response = await fetch(Object.values(preview)[0]);
       const blob = await response.blob();
+      // 拡張子
+      const fileExpand = blob.type.split('/')[1];
+
       // Firebase Storage に画像をアップロード
-      const ref = storage.ref().child('images').child('2021-05-01.png');
-      ref.put(blob).then((_snapshot) => {
+      const ref = storage
+        .ref()
+        .child('images')
+        .child(`2021-05-01.${fileExpand}`);
+      ref.put(blob).then((snapshot) => {
         console.log('アップロードが完了しました！');
       });
 
