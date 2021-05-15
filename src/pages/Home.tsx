54a176c60435d54c29f75d74aa1dcfe8356c39be
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-05-15 22:35:12 +0900

    [add] FireStore からの情報を非同期で取得し描画するよう実装

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,42 @@
-import React, { useState } from 'react';
-import { storage } from '../firebase/Firebase';
+import React, { useEffect, useState } from 'react';
+import { db } from '../firebase/Firebase';
+
+type ImageUrlsProps = {
+  date: string;
+  downloadUrl: string;
+};
 
 const Home = (): JSX.Element => {
-  const [imageUrl, setImageUrl] = useState<string>('');
-  const ref = storage.ref().child('images').child('2021-05-15.png');
-  ref.getDownloadURL().then((url: string) => {
-    setImageUrl(url);
-  });
+  // Firestore から取得した FireStrage 上の画像ダウンロード URL を非同期で格納する箱を作成
+  const [imageUrls, setImageUrls] = useState<ImageUrlsProps[]>([]);
+  // DOM 読み込み時に一回だけ imageUrls[] に、
+  // FireStorage に格納されている画像の downloadUrl 情報を格納する。
+  useEffect(() => {
+    // FireStore 上の ドキュメント[images] 配下にあるデータを取得する
+    const docRef = db.collection('images').get();
+    // 取得したデータを imageUrls[] に格納する
+    docRef.then((snapshot) => {
+      snapshot.forEach((doc) => {
+        setImageUrls((imageUrls) => [
+          ...imageUrls,
+          {
+            date: doc.id,
+            downloadUrl: doc.data().downloadUrl,
+          },
+        ]);
+      });
+    });
+  }, []);
+
   return (
     <>
       <h1>🌻 ひまわり図鑑 🌻</h1>
       <a href="/upload">アップロード画面へ</a>
-      <img src={imageUrl}></img>
+      <div>
+        {imageUrls.map((imageUrl) => {
+          return <div key={imageUrl.date}>{imageUrl.downloadUrl}</div>;
+        })}
+      </div>
     </>
   );
 };
