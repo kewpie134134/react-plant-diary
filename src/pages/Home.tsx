54c4c948456eca8868fa20adcf3044fcefbc8759
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-05-15 17:07:47 +0900

    [add] FireStorage から画像を取得するためのコードを記述

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { storage } from '../firebase/Firebase';
 
 const Home = (): JSX.Element => {
+  const [imageUrl, setImageUrl] = useState<string>('');
+  const ref = storage.ref().child('images').child('2021-05-13.png');
+  ref.getDownloadURL().then((url: string) => {
+    setImageUrl(url);
+  });
   return (
     <>
       <h1>🌻 ひまわり図鑑 🌻</h1>
       <a href="/upload">アップロード画面へ</a>
+      <img src={imageUrl}></img>
     </>
   );
 };
