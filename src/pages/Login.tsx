af7cacc3f15723340a996c22a3826aabe3e08d8d
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-05-16 19:55:23 +0900

    [fix] event の型定義を any で使用することにした

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,18 +7,13 @@ const Login = (): JSX.Element => {
   const { login } = useContext(AuthContext);
 
   // ログインボタンが押されたら発火する。
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  // event の型定義 React.FormEvent<HTMLFormElement> での値取得が困難なため、any を使用。
+  // eslint-disable-next-line
+  const handleSubmit = (event: any) => {
     // event 処理を一時停止させる
     event.preventDefault();
-
     // email と password の内容を取得
-    let email = '';
-    let password = '';
-    Array.prototype.forEach.call(event.currentTarget.elements, (element) => {
-      if (element.name === 'email') email = element.value;
-      if (element.name === 'password') password = element.value;
-    });
-
+    const { email, password } = event.target.elements;
     // AuthProvider の login 関数を使用
     login ? login(email, password) : null;
   };
