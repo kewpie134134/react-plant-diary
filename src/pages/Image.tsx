54e1dc9c771b7714f6425c7638a564ff99aa8cbd
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-05-23 13:23:45 +0900

    [update] エラー時の記述をコメントで記述

--- src/pages/Image.tsx
+++ src/pages/Image.tsx
@@ -4,9 +4,17 @@ import { useLocation } from 'react-router-dom';
 import Header from '../components/Header';
 
 const Image = (): JSX.Element => {
-  const location = useLocation<{ downloadUrl: string }>();
-  console.log(location.state.downloadUrl);
-  console.log(location.pathname);
+  try {
+    const location = useLocation<{ downloadUrl: string }>();
+    console.log(location.state.downloadUrl);
+    console.log(location.pathname);
+  } catch (e) {
+    // downloadUrl が取得できない場合に起こりうる。
+    // 1. firestore 上に画像が登録されていたらそれを取ってくる
+    // 2. 画像がアップロードされていない場合は、画像がない旨を知らせる
+    //    -> 画像アップロードページの UI を示す
+    console.log(e.message);
+  }
 
   return (
     <>
