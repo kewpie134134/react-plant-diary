c12e3659d5bc7cf6cfcbd49292d656986cf862d7
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-05-23 22:32:38 +0900

    [update] ホーム画面の画像を選択して、画像を再アップロードできるように修正

--- src/pages/EditStoredImage.tsx
+++ src/pages/EditStoredImage.tsx
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import Header from '../components/Header';
+import SelectImage from '../components/SelectImage';
+import PreviewRender from '../components/PreviewRender';
 
 const EditStoredImage = (): JSX.Element => {
-  let storedImage;
+  // ホーム画面上に表示されている、DB に格納されていた画像を格納するためのインスタンス
+  // ここでの storedImage には firestore に格納されている 'http://~~~' の文字列が入る。
+  const [storedImage, setStoredImage] = useState<string>('');
+  useEffect(() => {
+    setStoredImage(location.state.downloadUrl);
+  }, []);
+
+  // Link to で指定されたオブジェクト情報を取得する
   const location = useLocation<{ downloadUrl: string }>();
   // '/image/2021-01-01' が location.pathname に格納されているので、
-  // split を使用して 2 つめの '/' 以降の日付情報を取得する。
+  // split('/') を使用して 2 つめの '/' 以降の日付情報を取得する。
   const storedImageDate = location.pathname.split('/')[2];
-  try {
-    storedImage = location.state.downloadUrl;
-    // console.log(location.state.downloadUrl);
-  } catch (e) {
-    // downloadUrl が取得できない場合に起こりうる。
-    // 1. firestore 上に画像が登録されていたらそれを取ってくる
-    // 2. 画像がアップロードされていない場合は、画像がない旨を知らせる
-    //    -> 画像アップロードページの UI を示す
-    console.log(e.message);
-  }
+
+  // TODO:
+  // downloadUrl が取得できない場合に起こりうる。
+  // 1. firestore 上に画像が登録されていたらそれを取ってくる
+  // 2. 画像がアップロードされていない場合は、画像がない旨を知らせる
+  //    -> 画像アップロードページの UI を示す
 
   return (
     <>
       <Header pageName="画像編集" />
       <div>{storedImageDate}画像編集ページ</div>
-      <img src={storedImage} />
+      <SelectImage setPreview={setStoredImage} />
+      <PreviewRender preview={storedImage} />
     </>
   );
 };
