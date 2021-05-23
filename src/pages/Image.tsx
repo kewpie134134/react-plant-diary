1141ad692a7c76211313ae3735b577a3b47c1939
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-05-23 10:05:30 +0900

    [update] ヘッダー追加

--- src/pages/Image.tsx
+++ src/pages/Image.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+import Header from '../components/Header';
+
 type Image = {
   id: string;
 };
 
-const Image = () => {
+const Image = (): JSX.Element => {
   const { id }: Image = useParams();
 
   return (
     <>
+      <Header pageName="画像編集" />
       <div>{id}画像編集ページ</div>
     </>
   );
